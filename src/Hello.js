8559df93e91aa53449a8a576efc7cc9ab93b3463
Dany <dany@Danys-MacBook-Pro.local>
2020-12-16 10:32:16 -0600

    test tachyons style

--- src/Hello.js
+++ src/Hello.js
@@ -4,9 +4,9 @@ import './Hello.css';
 class Hello extends Component {
   render() {
     return (
-      <div>
+      <div className='f1 tc'>
         <h1>Hello World</h1>
-        <p>Welcome to our Site.</p>
+        <img src="https://miro.medium.com/max/512/1*DZCJUmhAQ4LgsXs1dNktOA.png" alt="meme says testing like a boss"></img>
       </div>
     );
   }
